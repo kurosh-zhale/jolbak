330aaa48e6af6ad07718efd4e14f9df574635479
KZ <kjreem.imagine@gmail.com>
2022-08-29 19:35:25 +0430

    [feat] created login

--- src/app/aaa/aaa.module.ts
+++ src/app/aaa/aaa.module.ts
@@ -1,14 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { AAARoutingModule } from './aaa-routing.module';
 
+/**App Modules */
+import { ReactiveFormsModule } from '@angular/forms';
 
+/**Material Modules */
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+
+/**Components */
+import { LoginComponent } from './components/login/login.component';
+
+const MaterialModules = [MatCardModule, MatInputModule];
 @NgModule({
-  declarations: [],
+  declarations: [LoginComponent],
   imports: [
     CommonModule,
-    AAARoutingModule
-  ]
+    AAARoutingModule,
+    ...MaterialModules,
+    ReactiveFormsModule,
+  ],
 })
-export class AAAModule { }
+export class AAAModule {}
--- src/app/aaa/components/login/login.component.ts
+++ src/app/aaa/components/login/login.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder,Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
+  loginForm = this.fb.group({
+    email: ['',Validators.required],
+    password: ['',Validators.required],
+  });
 
-  constructor() { }
+  constructor(private readonly fb: FormBuilder) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
+  onSubmit(): void {}
 }
